5b172783518abc61e4f71c34776cdad2d8bdd90f
sksadjad <skhoshkhou@sphereon.com>
2023-08-03 12:10:53 +0200

    chore: reverted the changes to profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -6,37 +6,19 @@ import { useWeb3 } from '@context/Web3'
 import ProfileProvider from '@context/Profile'
 import { useRouter } from 'next/router'
 import web3 from 'web3'
-import { useSelector } from 'react-redux'
-import { useOidcUser } from '@axa-fr/react-oidc'
-import { RootState } from '../../store'
-import { AuthenticationStatus } from '@components/Authentication/authentication.types'
-import { isOIDCActivated } from '../../../app.config'
 
 export default function PageProfile(): ReactElement {
   const router = useRouter()
   const { accountId } = useWeb3()
-  let authUser
-  if (JSON.parse(isOIDCActivated)) {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const { oidcUser } = useOidcUser()
-    authUser = oidcUser
-  }
-  const authenticationStatus = useSelector(
-    (state: RootState) => state.authentication.authenticationStatus
-  )
-
   const [finalAccountId, setFinalAccountId] = useState<string>()
   const [ownAccount, setOwnAccount] = useState(false)
-
+  // Have accountId in path take over, if not present fall back to web3
   useEffect(() => {
     async function init() {
       if (!router?.asPath) return
 
       // Path is root /profile, have web3 take over
-      if (
-        router.asPath === '/profile' &&
-        authenticationStatus !== AuthenticationStatus.OIDC
-      ) {
+      if (router.asPath === '/profile') {
         setFinalAccountId(accountId)
         setOwnAccount(true)
         return
@@ -50,16 +32,9 @@ export default function PageProfile(): ReactElement {
         const finalAccountId = pathAccount || accountId
         setFinalAccountId(finalAccountId)
       }
-
-      // Path has OIDC user information
-      if (authenticationStatus === AuthenticationStatus.OIDC && authUser) {
-        // Set the finalAccountId to the oidcUser username
-        setFinalAccountId(authUser.preferred_username)
-        setOwnAccount(true)
-      }
     }
     init()
-  }, [router, accountId, authenticationStatus, authUser])
+  }, [router, accountId])
 
   return (
     <Page
